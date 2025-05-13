0d0e4d99a58ce75d524a5e0e668d82ba6e434464
Anitagouda23 <anitagoudappanavar3@gmail.com>
2025-05-13 20:59:26 +0530

    Create invalid_op.c

---
+++ invalid_op.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+int main() 
+{
+    int a,b;
+    char op;
+ printf("enter the value of a,op and b");
+  scanf("%d%c%d",&a,&op,&b);
+  switch(op)
+  {
+  case '+': printf("%d", a+b); 
+             break;
+   case'-': printf("%d", a-b);
+              break;
+   case'*': printf("%d", a*b);
+              break;
+   case'/': printf("%d", a/b);
+              break;
+   default:printf("invalid operation");
+  }
+}
