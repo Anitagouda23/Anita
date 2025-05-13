38a90d7438a12cf85ee2db383804187a65fd9deb
Anitagouda23 <anitagoudappanavar3@gmail.com>
2025-05-13 20:06:15 +0530

    Create student_detail.c

---
+++ student_detail.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main() 
+{
+char name[]="anita";
+int age=20;
+char location[]="tumkur";
+    printf("name:%s\n",name);
+    printf("age:%d\n",age);
+    printf("location:%s\n",location);
+
+    return 0;
+}
