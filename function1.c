85ded8692b95fc7e502eb0a6ba4270f184c27634
Anitagouda23 <anitagoudappanavar3@gmail.com>
2025-05-13 15:42:11 +0530

    Create function1.c

---
+++ function1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int shridevi()                 //function declaration
+{
+  printf("anita\n");
+  printf("date of birth\n"); 
+  printf("age\n");
+  printf("mobile number\n");
+}
+  int main()
+{
+    shridevi();
+ return 0;
+}   
