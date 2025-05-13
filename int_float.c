0d5cc413aecf6a3f432a3c121b5cbc13aabd5760
Anitagouda23 <anitagoudappanavar3@gmail.com>
2025-05-13 20:17:44 +0530

    Create int_float.c

---
+++ int_float.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main() 
+{
+    int a;
+    float b;
+  printf("enter an integer and a float:");
+  scanf("%d%f",&a,&b);
+  printf("you entered:%d and %.2f\n",a,b);
+ return 0;
+}
