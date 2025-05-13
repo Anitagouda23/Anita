65bd26dc818e4140fd50723d9803b7273e1d41f6
Anitagouda23 <anitagoudappanavar3@gmail.com>
2025-05-13 20:25:56 +0530

    Create area_rectangle.c

---
+++ area_rectangle.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main() 
+{
+    
+    float l,b,area;
+    printf("enter the value of l and b");
+  scanf("%f%f",&l,&b);
+  area=l*b;
+  printf("area of rectangle is %f",area);
+  return 0;
+}
