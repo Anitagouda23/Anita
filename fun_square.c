38e6a7ff7d952b7b4f0c6cb9c64d3b80b8fd2296
Anitagouda23 <anitagoudappanavar3@gmail.com>
2025-05-14 22:29:05 +0530

    Create fun_square.c

---
+++ fun_square.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int sq(int n) {
+return n*n;
+}
+int main(){
+    int n=23;
+    int result=sq(n);
+    printf("ani\n");
+    printf("the square of %d is %d\n",n,result);
+return 0;
+}
