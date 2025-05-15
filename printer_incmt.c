4f63e4ede8f453a693a53dc881be4b82dfbbd91e
Anitagouda23 <anitagoudappanavar3@gmail.com>
2025-05-15 12:26:52 +0530

    Create printer_incmt.c

---
+++ printer_incmt.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int numbers[]={10,20,30,40,50};
+int*ptr=numbers;
+printf("value at ptr:%d\n",*ptr);
+ptr++;
+printf("value at ptr after increament:%d\n",*ptr);
+(*ptr)++;
+printf("value at ptr after increamenting the value:%d\n",*ptr);
+}
+ 
