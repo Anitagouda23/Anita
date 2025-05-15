daaf736efbaf7830b135e68c6e284b80d577176b
Anitagouda23 <anitagoudappanavar3@gmail.com>
2025-05-15 22:52:10 +0530

    Create 2d_array.c

---
+++ 2d_array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    
+    int a[3][4]={{1,2,3,4},{5,6,7,8},{3,4,5,6}};
+    int row,col;
+     for(row=0; row<3; row++){
+         for(col=0; col<4; col++){
+             printf("%d\n",a[row][col]);
+         }
+     }
+}
